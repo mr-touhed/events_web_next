1fcdd07b53656c6612f6ffc0d2701407f2e8ec8e
Touhed <mr.touhed.i@outlook.com>
2024-08-02 00:16:55 +0600

    deploy

--- src/components/Home/EventsInfo.jsx
+++ src/components/Home/EventsInfo.jsx
@@ -15,28 +15,28 @@ const EventsInfo = ({data}) => {
       <Tabs defaultValue="All" className="" onValueChange={(value) =>changeTab(value)}>
         <TabsList className="grid w-full md:grid-cols-4 grid-cols-2">
             {
-                tabList.map((tab,i) => <TabsTrigger  key={i} value={tab} className={`${activeMenu === tab ? "bg-primary text-white" : ""} hover:text-primary`}>{tab}</TabsTrigger>)
+                tabList?.map((tab,i) => <TabsTrigger  key={i} value={tab} className={`${activeMenu === tab ? "bg-primary text-white" : ""} hover:text-primary`}>{tab}</TabsTrigger>)
             }
           
         </TabsList>
         <TabsContent value="All" className="grid md:grid-cols-4 gap-6 mt-16">
                 {
-                    data.map((event,i) => <EventCard key={i} event={event}/>)
+                    data?.map((event,i) => <EventCard key={i} event={event}/>)
                 }
         </TabsContent>
         <TabsContent value="Conferences" className="grid grid-cols-4 gap-6">
                 {
-                    data.filter(event => event.catagory === "Conferences").map((event,i)=> <EventCard key={i} event={event}/>)
+                    data?.filter(event => event.catagory === "Conferences").map((event,i)=> <EventCard key={i} event={event}/>)
                 }
             </TabsContent>
         <TabsContent value="Workshops" className="grid grid-cols-4 gap-6">
                 {
-                    data.filter(event => event.catagory === "Workshops").map((event,i)=> <EventCard key={i} event={event}/>)
+                    data?.filter(event => event.catagory === "Workshops").map((event,i)=> <EventCard key={i} event={event}/>)
                 }
         </TabsContent>
         <TabsContent value="Concerts" className="grid grid-cols-4 gap-6">
                 {
-                    data.filter(event => event.catagory === "Concerts").map((event,i)=> <EventCard key={i} event={event}/>)
+                    data?.filter(event => event.catagory === "Concerts").map((event,i)=> <EventCard key={i} event={event}/>)
                 }
         </TabsContent>
       </Tabs>

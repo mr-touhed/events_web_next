24796831705d6fa2aa3648acbd8102c8cb86e773
Touhed <mr.touhed.i@outlook.com>
2024-08-02 01:36:00 +0600

    change token

--- src/app/dashboard/events/page.js
+++ src/app/dashboard/events/page.js
@@ -7,7 +7,7 @@ import { Suspense } from "react";
 const page = async  () => {
             const res = await fetch(`${process.env.NEXTAUTH_URL}/api/events`, {next:{tags:["events"]}});
             const result = await res.json();
-         
+         console.log(`${process.env.NEXTAUTH_URL}/api/events`);
     return (
         <div>
                 <Suspense fallback="Loading.....">
